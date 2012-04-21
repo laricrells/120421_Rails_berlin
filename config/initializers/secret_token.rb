caad4d568a93ee26c584c2dabcbad43ab77ea008
Your Name <laura.crellin@gmail.com>
2012-04-21 16:14:19 +0200

    Message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsgirlsBln::Application.config.secret_token = 'dc619c5e8f292e0c8d5192b93f012b7ba74cd4d23a23883e4749848fc419445e559306cb5519d9f5b8201d3be3738daaff3354520eb6c1eb0a1cdd74e2295c8a'
